4147cea580f7511b58a7869ee8b6ff45f6bc4f37
whoIsEllie <lukaskelemen@gmail.com>
2022-02-12 18:05:36 +0000

    Working music playing

--- README.md
+++
@@ -1,5 +0,0 @@
-# ImpostorBot
-
-A quick discord bot I've been working on in my free time. Current features include replacing certain strings with ascii emojis and resplonding to words with copypastas.
-
-If you'd like to use it, note that you need a config.json file which contains your Bot Token, or you can just replace the reference to the bot token file with your own bot token.
--- index.js
+++ index.js
@@ -1,157 +1,124 @@
-const discord = require('discord.js');
-const bot = new discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
-const config = require('./config.json');
-const replies = require('./replies.json');
-const PREFIX = '!';
+const Discord = require('discord.js')
+const { prefix, token } = require('./config.json')
+const DisTube = require('distube')
+const replies = require('./replies.json')
+const commands = require('./commands.json')
+const client = new Discord.Client({
+    intents: ['GUILDS', 'GUILD_VOICE_STATES', 'GUILD_MESSAGES'],
+})
+const distube = new DisTube.default(client)
 
-const ytdl = require("ytdl-core");
-const queue = new Map();
+client.once('ready', () => {
+    console.log('Logged in!')
 
+    distube.on('error', (channel, error) => {
+        console.error(error)
+        channel.send(`An error encoutered: ${error.slice(0, 1979)}`) // Discord limits 2000 characters in a message
+    })
+})
+
+client.on("ready", () => {
+    client.user.setStatus("online");
+    client.user.setActivity("!help, Happy Valentine's Day!", { type: "PLAYING" })
+})
 
 
 function randomChooser(in_array) {
-  // floor rounds down to the nearest integer and random is never 1
-  // therefore works for 1 length arrays too 
-  return in_array[Math.floor(in_array.length * Math.random())];
+    // floor rounds down to the nearest integer and random is never 1
+    // therefore works for 1 length arrays too
+    return in_array[Math.floor(in_array.length * Math.random())];
 }
 
-// sets the bots status
-bot.on("ready", () => {
-  bot.user.setStatus("online");
-  bot.user.setActivity('Happy Valentines day! Type ,help for a list of commands!', { type: "PLAYING" })
-})
 
-// reads an incoming message and reacts accordingly
-bot.on('message', async message => {
+client.on('messageCreate', message => {
+    // If we the bot is the author then we don't respond
     if (message.author.bot) return;
+    if (!message.content.startsWith(prefix) || message.author.bot){
 
-    let args = message.content.substring(PREFIX.length).split(" ");
-    const serverQueue = queue.get()
-
-
-    switch (args[0]) {
-        case 'play':
-            await execute(message, serverQueue);
-            return;
-
-        case 'skip':
-            skip(message, serverQueue);
-            return;
-
-        case 'stop':
-            stop(message, serverQueue);
-            return;
-    }
-});
-
-async function execute(message, serverQueue) {
-    const args = message.content.split(" ");
-
-    const voiceChannel = message.member.voice.channel;
-    if (!voiceChannel)
-        return message.channel.send(
-            "You need to be in a voice channel to play music!"
-        );
-    const permissions = voiceChannel.permissionsFor(message.client.user);
-    if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
-        return message.channel.send(
-            "I need the permissions to join and speak in your voice channel!"
-        );
-    }
-
-    const songInfo = await ytdl.getInfo(args[1]);
-    const song = {
-        title: songInfo.videoDetails.title,
-        url: songInfo.videoDetails.video_url,
-    };
-
-    if (!serverQueue) {
-        const queueContruct = {
-            textChannel: message.channel,
-            voiceChannel: voiceChannel,
-            connection: null,
-            songs: [],
-            volume: 5,
-            playing: true
-        };
-
-        queue.set(message.guild.id, queueContruct);
-
-        queueContruct.songs.push(song);
-
-        try {
-            var connection = await voiceChannel.join();
-            queueContruct.connection = connection;
-            play(message.guild, queueContruct.songs[0]);
-        } catch (err) {
-            console.log(err);
-            queue.delete(message.guild.id);
-            return message.channel.send(err);
+        var conditions = Object.keys(replies);
+        var in_string = message.content.toLowerCase();
+        if (conditions)
+            var found = conditions.find(element => in_string.includes(element));
+        if (found && message.author !== client.user) {
+            var chosen_arr = replies[found]; // replies for correct word
+            if (chosen_arr["delete"] && !chosen_arr["prefix-required"]) {
+                message.delete()
+            }
+            if (!chosen_arr["prefix-required"]) {
+                message.channel.send(randomChooser(chosen_arr["replies"]));
+            }
         }
-    } else {
-        serverQueue.songs.push(song);
-        return message.channel.send(`${song.title} has been added to the queue!`);
-    }
-}
-
-function skip(message, serverQueue) {
-    if (!message.member.voice.channel)
-        return message.channel.send(
-            "You have to be in a voice channel to stop the music!"
-        );
-    if (!serverQueue)
-        return message.channel.send("There is no song that I could skip!");
-    serverQueue.connection.dispatcher.end();
-}
-
-function stop(message, serverQueue) {
-    if (!message.member.voice.channel)
-        return message.channel.send(
-            "You have to be in a voice channel to stop the music!"
-        );
-
-    if (!serverQueue)
-        return message.channel.send("There is no song that I could stop!");
-
-    serverQueue.songs = [];
-    serverQueue.connection.dispatcher.end();
-}
-
-function play(guild, song) {
-    const serverQueue = queue.get(guild.id);
-    if (!song) {
-        serverQueue.voiceChannel.leave();
-        queue.delete(guild.id);
         return;
     }
 
-    const dispatcher = serverQueue.connection
-        .play(ytdl(song.url))
-        .on("finish", () => {
-            serverQueue.songs.shift();
-            play(guild, serverQueue.songs[0]);
+    const args = message.content
+        .slice(prefix.length)
+        .trim()
+        .split(' ')
+    const command = args.shift().toLowerCase()
+
+    if (command === 'play') {
+        distube.play(message.member.voice.channel, args.join(' '), {
+            message,
+            textChannel: message.channel,
+            member: message.member,
         })
-        .on("error", error => console.error(error));
-    dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
-    serverQueue.textChannel.send(`Start playing: **${song.title}**`);
-}
+        if (distube.getQueue(message)) {
+            message.channel.send(`Queued`);
+        } else {
+            message.channel.send(`Playing your song! ඞ`)
+        }
+    }
+    else if (command === 'stop') {
+        if (distube.getQueue(message)) {
+            distube.stop(message)
+            message.channel.send("Music stopped");
+        }
+        else {
+            message.channel.send("The queue is already empty");
+        }
+    }
+    else if (command === 'skip') {
+        if (distube.getQueue(message)) {
+            const queue = distube.getQueue(message)
+            if (queue.songs.length > 1) {
+                distube.skip(message)
+                message.channel.send("skipped track");
+            } else {
+                message.channel.send("No track to skip to! Pretty sus ඞ");
+            }
+        } else {
+            message.channel.send("No track to skip to! Pretty sus ඞ");
+        }
+    }
+    else if (command === 'pause'){
+        distube.pause(message)
+        message.channel.send("Paused the music!");
+    }
+    else if (command === 'resume'){
+        distube.resume(message)
+        message.channel.send("Resumed playback");
+    }
+    else if (command === 'queue'){
+        const queue = distube.getQueue(message);
+        message.channel.send('ඞ Current queue:\n' + queue.songs.map((song, id) =>
+            `**${id+1}**. ${song.name} - \`${song.formattedDuration}\``
+        ).join("\n"));
+    }
+    else {
+        var conditions = Object.keys(commands);
+        var in_string = message.content.toLowerCase();
+        if (conditions)
+            var found = conditions.find(element => in_string.includes(element));
+        if (found && message.author !== client.user) {
+            var chosen_arr = commands[found]; // replies for correct word
+            if (chosen_arr["delete"]) {
+                message.delete()
+            }
+            message.channel.send(randomChooser(chosen_arr["replies"]));
+        }
+    }
 
-/*var conditions = Object.keys(replies);
-var musickeyword = ",play"
-var in_string = ReceivedMessage.content.toLowerCase();
+})
 
-if (conditions)
-  var found = conditions.find(element => in_string.includes(element));
-
-if (found && ReceivedMessage.author !== bot.user) {
-  var chosen_arr = replies[found]; // replies for correct word
-
-  if (chosen_arr["delete"]) {
-    ReceivedMessage.delete()
-  }
-
-  ReceivedMessage.channel.send(randomChooser(chosen_arr["replies"]));
-}*/
-
-// handles login with API key
-bot.login(config["token"]);
+client.login(token)
